219795c265406d0ef12825009fcc77e697862b55
GabrielFrahm <gabriel_frahm@hotmail.com>
2024-05-04 17:29:36 -0300

    chore(swagger): add swagger lib

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const config = new DocumentBuilder()
+    .setTitle('e-commerce')
+    .setDescription('The e-commerce API description')
+    .setVersion('1.0')
+    .addTag('e-commerce')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+  await app.listen(3333);
 }
 bootstrap();
